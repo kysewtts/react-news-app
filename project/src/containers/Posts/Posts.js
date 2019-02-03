fed5de9f302824198b7f101e419f0b3d927b7ffc
Ujjwal Prakash <ujjwalprakash3@gmail.com>
2019-02-03 23:27:08 +0530

    added Posts.js

---
+++ project/src/containers/Posts/Posts.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import Spinner from '../../components/UI/Spinner/Spinner';
+
+class Posts extends Component{
+    render(){
+        const { posts } = this.props;
+        console.log(posts);
+        const postList = posts.length ? (
+            posts.map(post => {
+                return (
+                        <div className="post card" key = {post.title}>
+                            <div className="card-content">
+                                <Link to = {'/' + posts.indexOf(post)}>
+                                    <span className = "card-title">{post.title}</span>
+                                </Link>
+                                <p>{post.description}</p>
+                            </div>
+                        </div>
+                    )
+                }
+            )
+         ) : (
+             this.props.loading ? <Spinner /> : null
+         ) ;
+        return(
+            <div className="container" style = {{"marginTop": "50px"}}>
+                {postList}
+            </div>
+        );
+    }
+}
+
+
+const mapStateToProps = state => {
+    return {
+        posts: state.data,
+        loading: state.loading
+    }
+}
+
+
+export default connect(mapStateToProps)(Posts);
+
+
