215c02502a7fa432fe735e4d7957be51c124f6aa
Ujjwal Prakash <ujjwalprakash3@gmail.com>
2019-02-04 08:33:43 +0530

    edited App.js

--- project/src/App.js
+++ project/src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Navbar from './components/Layout/Navbar';
 import Input from './containers/Input/Input';
 import Post from './containers/Post/Post';
+import Footer from './components/Footer/Footer';
 
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
             <Route path = "/:post_id" component = {Post} />
             <Route path = "/" component = {Input} />
           </Switch>
+          <Footer />
         </div>
     );
   }
