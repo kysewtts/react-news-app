7030dbe80025049bbcff16dd57fa374084df3a54
Ujjwal Prakash <ujjwalprakash3@gmail.com>
2019-02-04 17:26:31 +0530

    edited footer

--- project/src/components/Footer/Footer.js
+++ project/src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ const footer = (props) => {
         <footer>
             <div className="footer-copyright">
                 <div className="container" style = {{"textAlign": "center", "border": "2px solid blue"}}>
-                 <a href = "https://newsapi.org/" style = {{"textDecoration": "none"}}>Powered By News API</a>
+                 <a href = "https://newsapi.org/" target = "blank" style = {{"textDecoration": "none"}}>Powered By News API</a>
                 </div>
             </div>
         </footer>
