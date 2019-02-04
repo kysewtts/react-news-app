886eb9af3eb997eba9472199a5edaa90b651c47f
Ujjwal Prakash <ujjwalprakash3@gmail.com>
2019-02-04 17:30:04 +0530

    edited getReducer

--- project/src/store/reducers/getReducer.js
+++ project/src/store/reducers/getReducer.js
@@ -10,7 +10,7 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch(action.type){
         case actionTypes.FETCH_START:
-            return updateObject(state, { loading: true, data: [] });
+            return updateObject(state, { loading: true, data: [], error: '' });
         case actionTypes.FETCH_FAIL:
             return updateObject(state, { loading: false, error: action.error});
         case actionTypes.FETCH_SUCCESS:
