3b2dba0b8504116ffd779e87db0c3881deafb9d9
Ujjwal Prakash <ujjwalprakash3@gmail.com>
2019-02-04 08:34:54 +0530

    added Footer

---
+++ project/src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const footer = (props) => {
+    return(
+        <footer>
+            <div className="footer-copyright">
+                <div className="container" style = {{"textAlign": "center"}}>
+                 Powered By News API
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default footer;
